31899959a1f50788d742bcc3c85387439cb38357
sandeep <vangarasandeep1996@gmail.com>
2022-10-16 17:41:25 +0530

    terraform-ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 2.7.0"
     }
   }
 }
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
