d9a3de52f19f9b2df327e8f73b362be149ab89df
sandeep <vangarasandeep1996@gmail.com>
2022-10-16 17:35:25 +0530

    terraform-ec2

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+key_name      = "user1"
+instance_type = "t2.medium"
+name          = "ansible"
+CIDR          = "170.0.0.0/16"
+AZ            = ["us-east-1a"]
+SUBNET        = ["170.0.0.0/19"]
+spot_type     = "persistent"
+spot_behavior = "stop"
+ami           = "ami-002070d43b0a4f171"
