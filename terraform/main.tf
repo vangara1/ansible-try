1a12bfb951b55cc1cdb62a811ae81cc8b7a1ba5e
sandeep <vangarasandeep1996@gmail.com>
2022-10-16 17:42:02 +0530

    terraform-ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.7.0"
+  default_tags {
+    tags = {
+      hashicorp-learn = "module-use"
     }
   }
 }
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
